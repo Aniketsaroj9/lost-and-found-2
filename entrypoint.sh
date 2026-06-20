eed909c6c8ff116f64161ec6044a4d6b390f3a1c
Aniketsaroj9 <aniketsaroj9@gmail.com>
2026-06-20 13:23:45 +0530

    Force MPM clean up at runtime in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,13 @@ sed -i "s/Listen 80/Listen ${PORT}/g" /etc/apache2/ports.conf
 sed -i "s/:80>/:${PORT}>/g" /etc/apache2/sites-available/000-default.conf
 
 # Start Apache in the foreground
+# forcefully remove all MPMs and enable only prefork right before starting Apache!
+rm -f /etc/apache2/mods-enabled/mpm_*.load
+rm -f /etc/apache2/mods-enabled/mpm_*.conf
+ln -s /etc/apache2/mods-available/mpm_prefork.load /etc/apache2/mods-enabled/mpm_prefork.load
+ln -s /etc/apache2/mods-available/mpm_prefork.conf /etc/apache2/mods-enabled/mpm_prefork.conf
+
+echo "Starting Apache with the following MPM modules in mods-enabled:"
+ls -la /etc/apache2/mods-enabled/mpm_*
+
 exec apache2-foreground
